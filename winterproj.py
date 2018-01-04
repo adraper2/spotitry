b87f39b26f92fb52e472fdab72cfa84261f12ed6
MAZimmermann <MAZimmermann26@gmail.com>
2018-01-04 15:55:44 -0500

    added code for checking spotitry playlist existence

--- winterproj.py
+++ winterproj.py
@@ -12,7 +12,6 @@ import spotipy.util as util
 #parameters for client information on Spotify's API
 scope = 'user-library-read user-top-read user-follow-read user-read-recently-played playlist-modify-public'
   
-
 with open('credentials.txt', 'r') as f:
     username = str.replace(f.readline(),'\n','')
     clientId = str.replace(f.readline(),'\n','')
@@ -20,14 +19,12 @@ with open('credentials.txt', 'r') as f:
     redirect = str.replace(f.readline(),'\n','')
     
     f.close()
-        
+
 #attempts access to Spotify's API - post redirected URL in the console
 token = util.prompt_for_user_token(username, scope,
                                    client_id=clientId, 
                                    client_secret=clientSecret,
                                    redirect_uri = redirect)
-
-
 
 def main():
     if token: #if access was granted
@@ -39,14 +36,14 @@ def main():
         for track in pList['items']:
             for genre in track['genres']:
                 if genre not in g_dict:
-                    g_dict[genre]=1
+                    g_dict[genre] = 1
                 else:
                     g_dict[genre] += 1
         g_list=[list(g_dict.keys())[list(g_dict.values()).index(x)]for x in sorted(list(g_dict.values()))[-5:]]
-        
+
         aList = sp.current_user_top_artists(limit=10, time_range='short_term')
         sim_list = []
-        for artist in aList['items']:    
+        for artist in aList['items']:
             for sim_art in sp.artist_related_artists(artist['id'])['artists']:
                 if sim_art['name'] not in sim_list:
                     sim_list.append(sim_art['name'])        
@@ -63,10 +60,24 @@ def main():
                     if songs['id'] not in trackIds:
                         trackIds.append(songs['id'])
                         print(songs['name'])
-        
-        #need to add: if playlist doesn't exist
-        playlist1 = sp.user_playlist_create(username, str(username + "'s awesome mix vol 1"), public=True)
-        
-        sp.user_playlist_add_tracks(username, playlist1['id'], trackIds[:100], position=None)
-            
+
+        print('\n')
+
+        #create spotitry playlist if it DOES NOT exist, or update spotitry playlist if it DOES exist
+        userPlaylists = sp.current_user_playlists(limit=50, offset=0)
+        userPlaylist = {}
+        for pls in userPlaylists['items']:
+            if pls['name'] not in userPlaylist:
+                userPlaylist[pls['name']] = pls['id']
+
+        if str(username + "'s awesome mix vol 1") not in userPlaylist:
+            print('Creating',str(username + "'s awesome mix vol 1"))
+            playlist1 = sp.user_playlist_create(username, str(username + "'s awesome mix vol 1"), public=True)
+            sp.user_playlist_add_tracks(username, playlist1['id'], trackIds[:100], position=None)
+        else:
+            playlist_id = userPlaylist[str(username + "'s awesome mix vol 1")]
+            print(str(username + "'s awesome mix vol 1"),'Already Created')
+            print('Updating',str(username + "'s awesome mix vol 1"))
+            sp.user_playlist_replace_tracks(username, playlist_id, trackIds[:100])
+
 main()
